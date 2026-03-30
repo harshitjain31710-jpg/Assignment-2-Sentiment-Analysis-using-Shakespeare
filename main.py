4216c98ab3da6027561cd9b4a5938146d6ef7a39
Harshit Jain <harshitjain31710@gmail.com>
2026-03-30 15:37:41 +0530

    Refactor sentiment analysis loop for readability

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Shakespeare Sentiment Analysis
+# Rule-based NLP system
+# Author: Harshit Jain
 positive_words=["powerful","shine","bright","living","memory","praise","room","posterity","arise","live","dwell","lovers"]
 negative_words=["unswept","besmeared","sluttish","wasteful","war","overturn","broils","fire","burn","death","enmity","oblivious","doom"]
 sentiment=0
@@ -5,13 +8,13 @@ lower_list=[]
 txt=open("H:\Assignment-2-Sentiment-Analysis-using-Shakespeare\sonet.txt","r")
 output=open("H:\Assignment-2-Sentiment-Analysis-using-Shakespeare\output.txt","w")
 txt_list=txt.readlines()
-for i in txt_list:
-    i=i.lower()
-    split_list=i.split()
-    for j in split_list:
-        if j in positive_words:
+for line in txt_list:
+    line=line.lower()
+    split_list=line.split()
+    for word in split_list:
+        if word in positive_words:
             sentiment+=1
-        if j in negative_words:
+        if word in negative_words:
             sentiment-=1
     if sentiment>0:
         output.write(f"line={i},sentiment=positive\n")
