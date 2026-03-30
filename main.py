72a31f5b6ea53cd014a053e4964da654f06f89d4
Harshit Jain <harshitjain31710@gmail.com>
2026-03-30 16:06:41 +0530

    Fix typos and improve comments in main.py

--- main.py
+++ main.py
@@ -3,38 +3,37 @@
 # Author: Harshit Jain
 
 
-#Block-1 Intoducing variables
+#Block-1 Introducing variables
 positive_words=["powerful","shine","bright","living","memory","praise","room","posterity","arise","live","dwell","lovers"]#List of positive words
 negative_words=["unswept","besmeared","sluttish","wasteful","war","overturn","broils","fire","burn","death","enmity","oblivious","doom"]#List of negative words
 sentiment=0
-lower_list=[]
 
 #Block2-Opening files
-txt=open("H:\Assignment-2-Sentiment-Analysis-using-Shakespeare\sonet.txt","r")#Opening the text file containing the sonet
-output=open("H:\Assignment-2-Sentiment-Analysis-using-Shakespeare\output.txt","w")#Opening the text file to store output in
+txt=open("sonnet.txt","r")#Opening the text file containing the sonnet in read mode
+output=open("output.txt","w")#Opening the text file to store output in write mode
 
 #Block3-Preprocessing on given sonet
-txt_list=txt.readlines()#braking text into lines
+txt_list=txt.readlines()#Breaking text into lines
 for line in txt_list:
     line=line.lower()#Lowercasing 
-    split_list=line.split()#Breaking into individual words
+    words=line.split()#Breaking into individual words
    
     #Block4-Assigning sentiment value
-    for word in split_list:
-        if word in positive_words:#Chechking if word is positive 
+    for word in words:
+        if word in positive_words:#Checking if word is positive 
             sentiment+=1
-        if word in negative_words:#Chechking if word is negative 
+        elif word in negative_words:#Checking if word is negative 
             sentiment-=1
 
     #Block5-Writting output
     if sentiment>0:#Checking sentiment value of respective line
-        output.write(f"line={i},sentiment=positive\n")#Storing line and sentiment in output.txt file
-    if sentiment<0:#Checking sentiment value of respective line
-        output.write(f"line={i},sentiment=negative\n")#Storing line and sentiment in output.txt file
-    if sentiment==0:#Checking sentiment value of respective line
-        output.write(f"line={i},sentiment=neutral\n")#Storing line and sentiment in output.txt file
-    sentiment=0#resetting sentiment value for eachrespective line
+        output.write(f"line:{line},sentiment:positive\n")#Storing line and sentiment in output.txt file
+    elif sentiment<0:#Checking sentiment value of respective line
+        output.write(f"line:{line},sentiment:negative\n")#Storing line and sentiment in output.txt file
+    elif sentiment==0:#Checking sentiment value of respective line
+        output.write(f"line:{line},sentiment:neutral\n")#Storing line and sentiment in output.txt file
+    sentiment=0#resetting sentiment value for each respective line
 
 #Block6-Closing files
-txt.close()#Closing text file containing sonet
+txt.close()#Closing text file containing sonnet
 output.close()#Closing text file containing output
